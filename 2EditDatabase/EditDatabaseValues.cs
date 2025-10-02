ffd8c181034327df38f5e2fed4977d0b13089b92
Chomp <dev@dev.sp-tarkov.com>
2025-10-02 22:56:10 +0100

    Updated nuget packages + ref fix

--- 2EditDatabase/EditDatabaseValues.cs
+++ 2EditDatabase/EditDatabaseValues.cs
@@ -1,6 +1,7 @@
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Enums;
+using SPTarkov.Server.Core.Models.Enums.Hideout;
 using SPTarkov.Server.Core.Models.Spt.Mod;
 using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Services;
